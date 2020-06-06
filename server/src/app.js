77624b168bd1c1dd7c5bf92888f4d635335ca951
Slav Lazarevic <slavlazar@gmail.com>
2020-06-06 13:34:12 +1000

    Created express app core

---
+++ server/src/app.js
@@ -0,0 +1,32 @@
+import cors from 'cors';
+import express from 'express';
+import compression from 'compression';
+import helmet from 'helmet';
+
+import getCorsOptions from './cors/getCorsOptions';
+// Middlewares
+import errorHandlerMiddleware from './middleware/errorHandler';
+import requestLoggingMiddleware from './middleware/requestLogging';
+
+const app = express();
+
+app.use(express.json({ limit: '50mb' }));
+
+const corsOptions = getCorsOptions(process.env.NODE_ENV);
+app.use(cors(corsOptions));
+
+app.use(compression());
+app.use(helmet());
+
+app.use(requestLoggingMiddleware);
+
+app.use(errorHandlerMiddleware);
+
+// 404 handler
+app.use((_, res) => {
+  res.status(404).json({
+    message: 'Not Found. The resource you are looking for does not exist!',
+  });
+});
+
+export default app;
